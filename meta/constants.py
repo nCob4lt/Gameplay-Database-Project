16728a9e37bf382abd73f76ac9408fef6d82d6d8
Mael <maelpotvain@gmail.com>
2025-11-10 09:25:29 +0100

    Adding constant file

---
+++ meta/constants.py
@@ -0,0 +1,16 @@
+"""
+
+Constants for the Gameplay Database Project
+
+"""
+
+import discord
+
+PROJECT_NAME = "Gameplay Database"
+VERSION = "1.0.0"
+PYTHON_VERSION = "3.11.4"
+AUTHOR = "Cobalt"
+DEFAULT_COLOR = discord.Color.dark_grey()
+DB_PATH = "gpdb.db"
+LOG_PATH = "logs/latest.log"
+ITEMS_PER_PAGE = 10
